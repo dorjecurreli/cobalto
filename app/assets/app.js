872827aec7ae343d5f69929cdf92b5eecab05cac
dorje.curreli <curreli@publicplan.de>
2024-06-05 19:20:45 +0200

    add grid shuffle

--- app/assets/app.js
+++ app/assets/app.js
@@ -8,3 +8,59 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import 'bootstrap'
 import './styles/app.css';
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carousels = document.querySelectorAll('.carousel');
+    const completedCarousels = new Set();
+    const totalCarousels = carousels.length;
+    const slideIndexes = {};
+
+    carousels.forEach(carousel => {
+        const artistId = carousel.getAttribute('data-artist-id');
+        slideIndexes[artistId] = 0;
+
+        carousel.addEventListener('slid.bs.carousel', function(event) {
+            const items = carousel.querySelectorAll('.carousel-item');
+            const activeIndex = Array.from(items).indexOf(event.relatedTarget);
+
+            if (activeIndex === 0) {
+                completedCarousels.add(artistId);
+            }
+
+            slideIndexes[artistId] = activeIndex;
+
+            if (completedCarousels.size === totalCarousels) {
+                shuffleGrid();
+                completedCarousels.clear();
+            }
+        });
+    });
+
+    function shuffleGrid() {
+        var parent = document.getElementById('artists-grid');
+        var items = Array.prototype.slice.call(parent.children);
+
+        items.forEach(function(item) {
+            item.classList.add('shuffle-leave');
+        });
+
+        setTimeout(function() {
+            var shuffledItems = items.sort(function() {
+                return 0.5 - Math.random();
+            });
+            shuffledItems.forEach(function(item) {
+                parent.appendChild(item);
+                item.classList.remove('shuffle-leave');
+                item.classList.add('shuffle-enter');
+            });
+
+            setTimeout(function() {
+                shuffledItems.forEach(function(item) {
+                    item.classList.remove('shuffle-enter');
+                });
+            }, 50);
+        }, 500);
+    }
+});
