f994b8c13415073d2fe00520f902f42c56f5b84e
dorje.curreli <curreli@publicplan.de>
2024-08-17 23:13:17 +0200

    add monolog

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
